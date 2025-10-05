ca7b0ba938e4959db71365ec25d4684e84801a29
ace <falls.16177@gmail.com>
2025-10-05 15:10:23 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
+import  java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Variables
+        double width;
+        double height;
+        // loops
+        while(true){
+            System.out.print("Enter width: ");
+            if (input.hasNextDouble()){
+                width = input.nextDouble();
+                break;
+            } else {
+                System.out.print("Invalid input try again: ");
+                input.next();
+            }
+        }
+
+        while(true){
+            System.out.print("Enter height: ");
+            if (input.hasNextDouble()){
+                height = input.nextDouble();
+                break;
+            } else  {
+                System.out.print("Invalid input try again: ");
+                input.next();
+            }
+        }
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+
+        System.out.println("Area of rectangle: " + area);
+        System.out.println("Perimeter of rectangle: " + perimeter);
+    }
 }
